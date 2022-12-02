9157e6a18f3d651ff62a793d7a757deea3756080
Carla Aluvai <carngie@gmail.com>
2022-12-02 14:15:17 +0300

    setting up database

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['techportfolio-env.eba-seppabie.us-west-2.elasticbeanstalk.com/']
 
 # Application definition
 
