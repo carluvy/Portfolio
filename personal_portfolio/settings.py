d8d44f99c76f128e3d10d0e9154ae47e04b1824d
Carla Aluvai <carngie@gmail.com>
2022-12-02 03:32:43 +0300

    added aws cname

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-portfolio-dev.us-west-2.elasticbeanstalk.com']
 
 # Application definition
 
