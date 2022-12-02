3d60e38e9c11cbe987f3b7b646ec2d4b658820cd
Carla Aluvai <carngie@gmail.com>
2022-12-02 13:29:50 +0300

    more housekeeping

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -169,8 +169,8 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
-    EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-    EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 
     AUTHENTICATION_BACKENDS = [
         'django.contrib.auth.backends.ModelBackend',
