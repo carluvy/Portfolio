e63548e3cd41cd61c7f6d8fc4168d3b83c37855e
Carla Aluvai <carngie@gmail.com>
2022-12-02 16:01:11 +0300

    mods

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -34,7 +34,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-ie#35@ev2+ode9f8m(h2ooou$p^(aj*2a+m316u@1e*p1f_lab")
+SECRET_KEY = os.environ["django-insecure-ie#35@ev2+ode9f8m(h2ooou$p^(aj*2a+m316u@1e*p1f_lab"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
