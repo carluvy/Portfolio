518f2058f4c6db0f52b43999198ced8a19b5319e
Carla Aluvai <carngie@gmail.com>
2022-12-05 17:07:39 +0300

    fixed superuser issues

--- build.sh
+++ build.sh
@@ -10,4 +10,3 @@ pip3 install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --username=carla  --no-input --email=admin@admin.com
