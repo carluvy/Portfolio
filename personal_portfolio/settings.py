3c4e0c27b58f3ea9b552635db4d2de5c63d642e5
Carla Aluvai <carngie@gmail.com>
2022-12-05 18:02:41 +0300

    fix img path

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -175,7 +175,7 @@ STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "projects/static", ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
+FILE_PATH_FIELD_DIRECTORY = 'projects/static/img'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- projects/models.py
+++ projects/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 
 # Create your models here.
+from personal_portfolio import settings
+
+
 class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="/img")
+    image = models.FilePathField(path=settings.FILE_PATH_FIELD_DIRECTORY)
 
     def __str__(self) -> str:
         return f"{self.title}: {self.description}"
