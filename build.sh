903ce1d48b36c0c8303e9c338b75be1ec50bf45b
Carla Aluvai <carngie@gmail.com>
2022-12-05 12:31:36 +0300

    housekeeping

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ pip3 install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --username
+python manage.py createsuperuser --username=admin
