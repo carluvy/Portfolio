b244200aefd81c401f19254333fd36ef1274b61d
Carla Aluvai <carngie@gmail.com>
2022-12-05 17:29:38 +0300

    add staticfiles url

--- personal_portfolio/urls.py
+++ personal_portfolio/urls.py
@@ -22,20 +22,22 @@ from django.conf import settings
 from blog import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("projects.urls")),
-    path("blog/", include("blog.urls")),
-    path("user/", include("users.urls")),
-    path("password_reset/done/",
-         auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
-         name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',
-         auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm.html"),
-         name='password_reset_confirm'),
-    path('reset/done/',
-         auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
-         name='password_reset_complete'),
-    path("accounts/", include("allauth.urls")),
-    # path("search/", views.search_blogs(search_params='data', request=raw_input), name="search_results"),
+                  path("admin/", admin.site.urls),
+                  path("", include("projects.urls")),
+                  path("blog/", include("blog.urls")),
+                  path("user/", include("users.urls")),
+                  path("password_reset/done/",
+                       auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'),
+                       name='password_reset_done'),
+                  path('reset/<uidb64>/<token>/',
+                       auth_views.PasswordResetConfirmView.as_view(
+                           template_name="registration/password_reset_confirm.html"),
+                       name='password_reset_confirm'),
+                  path('reset/done/',
+                       auth_views.PasswordResetCompleteView.as_view(
+                           template_name='registration/password_reset_complete.html'),
+                       name='password_reset_complete'),
+                  path("accounts/", include("allauth.urls")),
+                  # path("search/", views.search_blogs(search_params='data', request=raw_input), name="search_results"),
 
-]
+              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
