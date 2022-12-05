723aa37e3f9cfe8e941a651ec489d6d7fc7bac1a
Carla Aluvai <carngie@gmail.com>
2022-12-05 13:52:38 +0300

    fix admin issues

--- build.sh
+++ build.sh
@@ -10,4 +10,4 @@ pip3 install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --username=admin --email=admin@admin.com --no-input
+#python manage.py createsuperuser --username=admin --email=admin@admin.com --no-input
--- projects/management/commands/createsu.py
+++ projects/management/commands/createsu.py
@@ -6,4 +6,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(username="admin", password="admin", email="admin@admin.com")
+            User.objects.create_superuser(username="admin", password="admincarla", email="admin@admin.com")
