aff35b4f67d88a0607b83150b0972b5edbf8a76c
Carla Aluvai <carngie@gmail.com>
2022-12-05 14:41:40 +0300

    fix errors

--- projects/management/commands/createsu.py
+++ projects/management/commands/createsu.py
@@ -1,3 +1,5 @@
+import os
+
 from django.contrib.auth.models import User
 from django.core.management import BaseCommand
 
@@ -6,4 +8,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser(username="admin", password="admincarla", email="admin@admin.com")
+            DJANGO_SU_NAME = os.environ.get("DJANGO_SUPERUSER")
+            DJANGO_SU_EMAIL = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
+            DJANGO_SU_PASSWORD = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+            super_user = User.objects.create_superuser(
+                username=DJANGO_SU_NAME,
+                password=DJANGO_SU_PASSWORD,
+                email=DJANGO_SU_EMAIL)
+
+            super_user.save()
