972e93e4a062422116abd6e0ad796b89f7083c35
Carla Aluvai <carngie@gmail.com>
2022-12-05 11:30:30 +0300

    su fix errors

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ pip3 install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py createsuperuser --username=MrsSteele --email=carngie@gmail.com [--noinput]
+python manage.py createsuperuser --username=MrsSteele --email=carlaluvai@outlook.com --noinput
--- projects/management/commands/createsu.py
+++ projects/management/commands/createsu.py
@@ -6,4 +6,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         if not User.objects.filter(username="admin").exists():
-            User.objects.create_superuser("MrsSteel", "waSteel")
+            User.objects.create_superuser(username="MrsSteel", password="kimitta", email="carlaluvai@outlook.com")
