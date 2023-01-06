332b6f28211badfca64eb101828d0020ef4e7b3d
Carla Aluvai <carngie@gmail.com>
2023-01-06 10:55:59 +0300

    modified filepath

--- projects/models.py
+++ projects/models.py
@@ -9,7 +9,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="images")
+    image = models.FilePathField(path="")
 
     def __str__(self) -> str:
         return f"{self.title}: {self.description}"
