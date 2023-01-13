7259925f8325256e416cc53c12c77a74aa6c7194
Carla Aluvai <carngie@gmail.com>
2023-01-13 16:24:14 +0300

    update buildscript

--- build.sh
+++ build.sh
@@ -12,5 +12,5 @@ pip3 install -r requirements.txt
 python manage.py makemigrations
 python manage.py migrate
 python manage.py createsuperuser --noinput
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 
