cb203a0279f3f91e406b9bdf6678224acf00a5b5
Carla Aluvai <carngie@gmail.com>
2023-01-13 16:47:34 +0300

    updated build.sh

--- build.sh
+++ build.sh
@@ -11,6 +11,6 @@ pip3 install -r requirements.txt
 
 python manage.py makemigrations
 python manage.py migrate
-python manage.py createsuperuser
+ython manage.py createsuperuser --username=carla  --no-input --email=carngie@gmail.com
 python manage.py collectstatic --no-input
 
